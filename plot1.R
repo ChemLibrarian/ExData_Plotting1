371ae23ca67a7e5d1302ecf53beca19c07ec9929
Ye Li <liye@umich.edu>
2015-04-13 17:15:35 -0400

    Fix plot1 y axis label
    Fixed plot1 y axis label. Committed after submission. Feel free to
    disregard this commit for grading purpose. Thanks!

--- plot1.R
+++ plot1.R
@@ -14,6 +14,6 @@ plot1 <- function () {
         
         #plot the first plot and save it to png file
         png(file = "plot1.png", width = 480, height = 480)
-        hist(HPConsum2d$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
+        hist(HPConsum2d$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
         dev.off()
 }
